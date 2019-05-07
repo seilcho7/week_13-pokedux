29832626fb4b02be3fec2661309cd1d0b9a45fe3
Seil Cho <seilcho7@hotmail.com>
2019-05-07 11:24:09 -0400

    limits each reducer then combines them as rootReducer

--- src/index.js
+++ src/index.js
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+
 
 // =============================================================================
 // STATE
 
-import initialState from './base.json';
-console.log(initialState);
+import { createStore, combineReducers } from 'redux';
+// import initialState from './base.json';
+import initialCards from './base.json';
+// console.log(initialState);
+
+const VISIBILITY_ALL = 'all';
+const VISIBILITY_CAUGHT = 'caught';
+const VISIBILITY_UNCAUGHT = 'uncaught';
+
+const initialState = {
+    ...initialCards,
+    visibilityFilter: VISIBILITY_ALL
+};
 
 // the state is an object
 // with a cards property
 // which is an array of objects
 // { cards: [ {}, {}, {} ] }
+
 
 // =============================================================================
 // ACTIONS + ACTION CREATORS
@@ -31,14 +43,57 @@ function catchCard(id) {
 }
 window.catchCard = catchCard;
 
+const ACTION_VISIBILITY_ALL = VISIBILITY_ALL;
+const ACTION_VISIBILITY_CAUGHT = VISIBILITY_CAUGHT;
+const ACTION_VISIBILITY_UNCAUGHT = VISIBILITY_UNCAUGHT;
+
+function setVisibilityAll() {
+    return {
+        type: ACTION_VISIBILITY_ALL
+    };
+}
+
+function setVisibilityCaught() {
+    return {
+        type: ACTION_VISIBILITY_CAUGHT
+    };
+}
+
+function setVisibilityUncaught() {
+    return {
+        type: ACTION_VISIBILITY_UNCAUGHT
+    };
+}
+
+window.setVisibilityAll = setVisibilityAll;
+window.setVisibilityCaught = setVisibilityCaught;
+window.setVisibilityUncaught = setVisibilityUncaught;
+
+
 // =============================================================================
 // REDUCER
 
-function cards(state=initialState, action={type: ''}) {
-    console.log(`cards got called with ${action.payload}`)
+// cards reducer manages an array
+function cards(state=initialState.cards, action={type: ''}) {
+    console.log(`cards got called with ${action.payload}`);
     switch(action.type) {
         case ACTION_CATCH:
             // find the card, set it to "caught"
+            const newState = state.map(card => {
+                    if (card.id === action.payload.id) {
+                        return {
+                            ...card,
+                            isCaught: true
+                        }
+                    } else {
+                        return card;
+                    }
+                })
+            
+            // Whatever is returned by the reducer
+            // is automatically used by the store as
+            // the new version of state
+            return newState;
         break;
         default:
             return state;
@@ -46,10 +101,33 @@ function cards(state=initialState, action={type: ''}) {
     }
 }
 
+// visibility reducer manages a string
+function visibility(state=initialState.visibilityFilter, action={type: ''}) {
+    switch(action.type) {
+        case ACTION_VISIBILITY_ALL:
+            return VISIBILITY_ALL;
+        break;
+        case ACTION_VISIBILITY_CAUGHT:
+            return VISIBILITY_CAUGHT;
+        break;
+        case ACTION_VISIBILITY_UNCAUGHT:
+            return VISIBILITY_UNCAUGHT;
+        break;
+        default:
+            return state;
+        break;
+    }
+}
+
+const rootReducer = combineReducers({
+    cards: cards,
+    visibilityFilter: visibility
+});
+
 // =============================================================================
 // STORE
 
-const store = createStore(cards);
+const store = createStore(rootReducer);
 window.store = store;
 
 ReactDOM.render(<App />, document.getElementById('root'));
