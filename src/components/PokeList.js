fb705bdf8c4d95ed5fe03a0b1405b019b224f98f
Seil Cho <seilcho7@hotmail.com>
2019-05-08 08:06:20 -0400

    adds card image

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -5,35 +5,31 @@ function PokeList({visibilityFilter, cards, handleClick}) {
     const cardItems = cards.map((card) => {
         return <li key={card.id} onClick={() => {
             handleClick(card.id);
-        }}>{card.name}</li>
+        }}><img src={card.imageUrlHiRes}
+        /><br/>{card.name}</li>
     })
 
     const caughtItems = cards.filter((card) => {
         return card.isCaught === true
-    })
-
-    const caught = caughtItems.map((card) => {
+    }).map((card) => {
         return <li key={card.id} onClick={() => {
                 handleClick(card.id);
-        }}>{card.name}</li>
-    })
+        }}><img src={card.imageUrlHiRes}
+        /><br/>{card.name}</li>
+    });
 
     const uncaughtItems = cards.filter((card) => {
         return !card.isCaught
-    })
-
-    const uncaught = uncaughtItems.map((card) => {
+    }).map((card) => {
         return <li key={card.id} onClick={() => {
             handleClick(card.id);
-        }}>{card.name}</li>
-    })
+        }}><img src={card.imageUrlHiRes}
+        /><br/>{card.name}</li>
+    });
 
-
-    
-    
     return (
-        <ul>
-            {visibilityFilter === "all" ? cardItems : visibilityFilter ==="caught" ? caught : visibilityFilter === "uncaught" ? uncaught : null}
+        <ul className="list">
+            {visibilityFilter === "all" ? cardItems : visibilityFilter ==="caught" ? caughtItems : visibilityFilter === "uncaught" ? uncaughtItems : null}
         </ul>
     )
 }
